e61aaaac99c01aa8503cf658188337ad9d420b79
SimsonCult <63821433+SimsonCult@users.noreply.github.com>
2020-05-09 07:15:46 -0300

    Add files via upload

---
+++ precache-manifest.46e52ca31d470720dc8ccfa6fb8f1d86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675282b1e211b73e0efde849c5fe9b3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52a5e88f376a6b653444",
+    "url": "/static/css/2.96d2ed0c.chunk.css"
+  },
+  {
+    "revision": "0ea36de90b18ceac1a04",
+    "url": "/static/css/main.52345ff1.chunk.css"
+  },
+  {
+    "revision": "52a5e88f376a6b653444",
+    "url": "/static/js/2.67cad49b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.67cad49b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea36de90b18ceac1a04",
+    "url": "/static/js/main.85e481a7.chunk.js"
+  },
+  {
+    "revision": "64d1778a673a2438231d",
+    "url": "/static/js/runtime-main.e343705b.js"
+  },
+  {
+    "revision": "cb90c5bda3dbe4e16f7607c0c5213f3a",
+    "url": "/static/media/logo.cb90c5bd.svg"
+  }
+]);
